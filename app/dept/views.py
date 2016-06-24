e9e3055b83bfd75fdfe1eaf6643c636c1a51ab86
khong97 <kevin.zy.hong@gmail.com>
2016-06-24 01:39:51 -0500

    working pages

--- app/dept/views.py
+++ app/dept/views.py
@@ -7,12 +7,24 @@ from . import dept
 def departmentlist(dept):#, page=1):
     classes = db.session.query(models.Search).filter(models.Search.name0.like(str(dept) + " " +"%")).order_by(models.Search.name0).all()
     #classes = classes.paginate(1, 20, False).items
+    professors = db.session.query(models.Search).filter(models.Search.dept==str(dept)).order_by(models.Search.name0).all()
+    #professors= professors.paginate(1,20, False).items
     links_classes = []
+    links_professors = []
     for x in classes:
         temp = x.name0.split()
         links_classes.append((x.name0, x.name1,"/dept/"+temp[0]+"/class/"+temp[1]))
-    return render_template("departmentlist.html", links_classes = links_classes)
+    for x in professors:
+        temp = x.name0.replace(" ", "")
+        links_professors.append((x.name0, "/professor/" + temp))
+    return render_template("departmentlist.html", links_classes=links_classes, links_professors=links_professors)
 
 @dept.route('/<dept>/class/<classnum>')
 def classthing(dept, classnum):
-    return dept + classnum
+    reviews = db.session.query(models.Reviews).filter(models.Reviews.classname==(str(dept) + " " + str(classnum)))
+    stats = [db.session.query(db.func.avg(models.Reviews.easy)).filter(models.Reviews.classname==(str(dept) + " " + str(classnum)))[0],
+            db.session.query(db.func.avg(models.Reviews.quality)).filter(models.Reviews.classname==(str(dept) + " " + str(classnum)))[0]]
+    professors = []
+    for x in reviews.order_by(models.Reviews.professorname).distinct(models.Reviews.professorname):
+        professors.append(x.professorname)
+    return render_template("class.html", classname = str(dept) + " " + str(classnum), reviews=reviews.order_by(models.Reviews.date.desc()), stats=stats, professors=professors)
--- app/professor/views.py
+++ app/professor/views.py
@@ -1,6 +1,22 @@
 from flask import Blueprint, render_template
+from app import db, models
 from . import prof
+
+def addspace(coursecode):
+    y=0
+    for x in coursecode:
+        if x.isupper() and x is not coursecode[0]:
+            break
+        y+=1
+    return coursecode[0:y] + " " + coursecode[y:]
 
 @prof.route('/<professor>')
 def stuff(professor):
-  return professor
+    professor = addspace(professor)
+    reviews = db.session.query(models.Reviews).filter(models.Reviews.professorname==(str(professor)))
+    stats = [db.session.query(db.func.avg(models.Reviews.easy)).filter(models.Reviews.professorname==(str(professor)))[0],
+              db.session.query(db.func.avg(models.Reviews.quality)).filter(models.Reviews.professorname==(str(professor)))[0]]
+    classes = []
+    for x in reviews.order_by(models.Reviews.classname).distinct(models.Reviews.classname):
+        classes.append(x.classname)
+    return render_template("profpage.html", professorname = str(professor), reviews=reviews.order_by(models.Reviews.date.desc()), stats=stats, classes=classes)
