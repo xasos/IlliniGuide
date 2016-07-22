60190189ca3d182be1c3956b36ff30f8c46ba910
khong97 <kevin.zy.hong@gmail.com>
2016-07-22 12:22:39 -0500

    db backup

--- dev/metricfill.py
+++ dev/metricfill.py
@@ -7,10 +7,10 @@ for filename in glob.glob("data/ClassMetrics/*.csv"):
     i = 0
     for line in read:
         if i != 0:
-            db.session.add(models.Metrics(subject=line[1], number=line[2], title=line[3], section=line[4], term=line[5],
+            db.session.add(models.Metrics(subject=line[1], number=line[2], title=line[3], section=line[4], term=line[6],
                             instructor=line[7], aplus=line[8], a=line[9], aminus=line[10], bplus=line[11], b=line[12], bminus=line[13],
                             cplus=line[14], c=line[15], cminus=line[16], dplus=line[17], d=line[18], dminus=line[19], f=line[20],
-                            w=line[21], avg=line[22]))
+                            w=line[21], avg=line[22], type=line[5], crn=line[0]))
         i += 1
 
 db.session.commit()
